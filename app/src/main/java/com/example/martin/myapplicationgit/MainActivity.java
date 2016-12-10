fb0969339d20c54c54cbd3c44afda42eacbc37d6
Martín Silva Molina <msilvamolina@gmail.com>
2016-12-10 01:20:02 -0300

    nuevo cambio en rama

--- app/src/main/java/com/example/martin/myapplicationgit/MainActivity.java
+++ app/src/main/java/com/example/martin/myapplicationgit/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //con esto vamos a probar la nueva rama
+        //nuevoa cambio
     }
 }
