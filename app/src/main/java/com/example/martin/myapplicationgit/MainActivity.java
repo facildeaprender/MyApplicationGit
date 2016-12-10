8701b827aa2583a414c003afec72ad550139f816
Martín Silva Molina <msilvamolina@gmail.com>
2016-12-10 01:18:05 -0300

    Nueva rama

--- app/src/main/java/com/example/martin/myapplicationgit/MainActivity.java
+++ app/src/main/java/com/example/martin/myapplicationgit/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //este es un comentario para probar git
+        //con esto vamos a probar la nueva rama
     }
 }
